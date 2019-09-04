3a3ba71afc9e1e9d81d83dba4be7307173f65886
rahul191296 <47738055+rahul191296@users.noreply.github.com>
2019-09-04 13:49:09 +0530

    Update MyApp.java

--- ModAdmin/src/main/java/com/cognizant/app/MyApp.java
+++ ModAdmin/src/main/java/com/cognizant/app/MyApp.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.cognizant.*")
 @EntityScan(basePackages = "com.cognizant.entity")
 public class MyApp {
